ee1d88dee05f81ce6a9a5b8dbaca1cdee4cc3c93
Darian Bailey <darian@nftychat.xyz>
2024-02-20 21:31:54 -0500

    fix bug

--- pages/try-namestone.js
+++ pages/try-namestone.js
@@ -170,7 +170,7 @@ export default function Home() {
               labelText="Wallet Address"
               placeholderText="0x1232...  or slobo.eth"
               onChange={(e) => setWalletInput(e.target.value)}
-              value={setWalletInput}
+              value={WalletInput}
             />
             <WhiteInput
               labelText="Email"
