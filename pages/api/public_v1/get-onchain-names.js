57f5e370b3d0e428e2e10abda561312a8cdcb4eb
Darian Bailey <darian@nftychat.xyz>
2024-03-28 19:37:46 -0400

    improve nft naems

--- pages/api/public_v1/get-onchain-names.js
+++ pages/api/public_v1/get-onchain-names.js
@@ -8,7 +8,7 @@ const cors = Cors({
   origin: "*",
 });
 
-// Connect to the Arbitrum Sepolia testnet
+// Connect to the Arbitrum Sepolia testnet using the Alchemy RPC endpoint
 const web3 = new Web3(
   "https://arb-sepolia.g.alchemy.com/v2/J-2xMLR7YSCYeXGpN-Pj7JApRPeNawbP"
 );
@@ -19,32 +19,44 @@ const contract = new web3.eth.Contract(contractABI, contractAddress);
 async function handler(req, res) {
   // Check required parameters
   const domain = req.query.domain;
-  const address = req.query.address;
+  const addresses = req.query.addresses?.split(","); // Assuming addresses are comma-separated
 
   if (!domain) {
     return res.status(400).json({ message: "Missing required parameters" });
   }
 
-  const subDomainName = await sql`
-    SELECT name, keys FROM ponder."NftSubdomain" WHERE "domainName" = ${domain} AND "address" = ${address}
-  `;
+  let subDomainNames;
 
-  if (subDomainName.length === 0) {
-    return res.status(404).json({ message: "Subdomain not found" });
+  if (addresses && addresses.length > 0) {
+    subDomainNames = await sql`
+      SELECT name, keys FROM ponder."NftSubdomain" WHERE "domainName" = ${domain} AND "address" = ANY (${addresses})
+    `;
+  } else {
+    subDomainNames = await sql`
+      SELECT name, keys FROM ponder."NftSubdomain" WHERE "domainName" = ${domain}
+    `;
   }
 
-  const name = subDomainName[0].name;
-  const tokenId = await contract.methods.tokenFor(name).call();
-
-  const textRecords = {};
-  const keys = ["avatar", "description", "location", "com.twitter", "url"];
-
-  for (const key of keys) {
-    const value = await contract.methods.text(tokenId, key).call();
-    textRecords[key] = value;
+  if (subDomainNames.length === 0) {
+    return res.status(404).json({ message: "No subdomains found" });
   }
 
-  return res.status(200).json({ ...subDomainName[0], textRecords });
+  const result = [];
+
+  for (const { name, keys } of subDomainNames) {
+    const tokenId = await contract.methods.tokenFor(name).call();
+
+    const textRecords = {};
+    const keysList = keys.slice(1, -1).split(","); // Remove surrounding brackets and split by comma
+    for (const key of keysList) {
+      const value = await contract.methods.text(tokenId, key.trim()).call(); // Trim whitespace from keys
+      textRecords[key.trim()] = value;
+    }
+
+    result.push({ name, textRecords });
+  }
+
+  return res.status(200).json(result);
 }
 
 export default cors(handler);
