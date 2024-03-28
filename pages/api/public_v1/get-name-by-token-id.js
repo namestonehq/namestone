f921c601f17e98934700935626ad09a17b1d25e1
Darian Bailey <darian@nftychat.xyz>
2024-03-27 21:47:50 -0400

    name by token id

---
+++ pages/api/public_v1/get-name-by-token-id.js
@@ -0,0 +1,29 @@
+import sql from "../../../lib/db";
+import Cors from "micro-cors";
+
+const cors = Cors({
+  allowMethods: ["GET", "HEAD", "POST", "OPTIONS"],
+  origin: "*",
+});
+
+async function handler(req, res) {
+  // Check required parameters
+  const domain = req.query.domain;
+
+  const tokenId = req.query.token_id;
+
+  if (!domain || !tokenId) {
+    return res.status(400).json({ message: "Missing required parameters" });
+  }
+  const subDomainName = await sql`
+    SELECT name FROM ponder."NftSubdomain WHERE domainName = ${domain} AND tokenId = ${tokenId}
+  `;
+
+  if (subDomainName.length === 0) {
+    return res.status(404).json({ message: "Subdomain not found" });
+  }
+
+  return res.status(200).json(subDomainName);
+}
+
+export default cors(handler);
