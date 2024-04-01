3ca3bb03771d73cfe850a49dbe81c99148bfacef
Darian Bailey <darian@nftychat.xyz>
2024-04-01 14:37:04 -0400

    multicall

--- pages/api/public_v1/get-onchain-names.js
+++ pages/api/public_v1/get-onchain-names.js
@@ -1,7 +1,8 @@
 import sql from "../../../lib/db";
 import Cors from "micro-cors";
 import { CypherPunk } from "../../../data/contracts/CypherPunk";
-import Web3 from "web3";
+import { createPublicClient, http } from "viem";
+import { multicall } from "@viem/multicall";
 
 const cors = Cors({
   allowMethods: ["GET", "HEAD", "POST", "OPTIONS"],
@@ -9,12 +10,15 @@ const cors = Cors({
 });
 
 // Connect to the Arbitrum Sepolia testnet using the Alchemy RPC endpoint
-const web3 = new Web3(
-  "https://arb-sepolia.g.alchemy.com/v2/J-2xMLR7YSCYeXGpN-Pj7JApRPeNawbP"
-);
+const client = createPublicClient({
+  chain: "arbitrum-sepolia",
+  transport: http(
+    "https://arb-sepolia.g.alchemy.com/v2/J-2xMLR7YSCYeXGpN-Pj7JApRPeNawbP"
+  ),
+});
+
 const contractAddress = "0xcdb7fafde2212ec26f58f275fedf07a6ef69814c";
 const contractABI = CypherPunk;
-const contract = new web3.eth.Contract(contractABI, contractAddress);
 
 async function handler(req, res) {
   // Check required parameters
@@ -26,14 +30,13 @@ async function handler(req, res) {
   }
 
   let subDomainNames;
-
   if (addresses && addresses.length > 0) {
     subDomainNames = await sql`
-      SELECT name, address, keys FROM ponder."NftSubdomain" WHERE "domainName" = ${domain} AND "address" = ANY (${addresses})  order by "registeredAt" desc
+      SELECT name, address, "tokenId", keys FROM ponder."NftSubdomain" WHERE "domainName" = ${domain} AND "address" = ANY (${addresses}) order by "registeredAt" desc
     `;
   } else {
     subDomainNames = await sql`
-      SELECT name, address, keys FROM ponder."NftSubdomain" WHERE "domainName" = ${domain} order by "registeredAt" desc
+      SELECT name, address, "tokenId", keys FROM ponder."NftSubdomain" WHERE "domainName" = ${domain} order by "registeredAt" desc
     `;
   }
 
@@ -41,19 +44,29 @@ async function handler(req, res) {
     return res.status(200).json([]);
   }
 
+  const contract = new multicall({
+    contracts: [
+      {
+        address: contractAddress,
+        abi: contractABI,
+        functionName: "text",
+      },
+    ],
+    publicClient: client,
+  });
+
   const result = [];
-
-  for (const { name, address, keys } of subDomainNames) {
-    const tokenId = await contract.methods.tokenFor(name).call();
-
+  for (const { name, address, tokenId, keys } of subDomainNames) {
     const keysList = keys.slice(1, -1).split(",");
-    const promises = keysList.map(async (key) => {
-      const value = await contract.methods.text(tokenId, key.trim()).call();
-      return { key: key.trim(), value };
-    });
+    const promises = keysList.map((key) =>
+      contract.call({
+        args: [tokenId, key.trim()],
+        shouldResolveResult: true,
+      })
+    );
 
     const textRecords = (await Promise.all(promises)).reduce(
-      (acc, { key, value }) => ({ ...acc, [key]: value }),
+      (acc, value, index) => ({ ...acc, [keysList[index].trim()]: value }),
       {}
     );
 
