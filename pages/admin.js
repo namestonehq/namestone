33af027d7bcaa3a8768d32b2e9d4a63a4bacf2f8
aslobodnik <aslobodnik@gmail.com>
2024-11-26 16:12:55 -0500

    add links and addresses

--- pages/admin.js
+++ pages/admin.js
@@ -489,51 +489,315 @@ function AddNameModal({
   nameErrorMsg,
   addressErrorMsg,
 }) {
+  const [activeTab, setActiveTab] = useState("profile");
+  const [showSubname, setShowSubname] = useState(false);
+  const tabs = ["profile", "links", "addresses", "custom"];
+
+  const TAB_CONTENT = {
+    profile: (
+      <div className="flex flex-col mt-4">
+        {/* Avatar */}
+        <div className="flex flex-row justify-between">
+          <div className="mb-2 text-sm font-bold text-brownblack-700">
+            Avatar
+          </div>
+          <div className="text-sm text-red-500">{nameErrorMsg}</div>
+        </div>
+        <input
+          className="w-full px-4 py-2 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+          placeholder="https://"
+        />
+        {/* Description */}
+        <div className="flex flex-row justify-between">
+          <div className="mb-2 text-sm font-bold text-brownblack-700">
+            Description
+          </div>
+          <div className="text-sm text-red-500">{nameErrorMsg}</div>
+        </div>
+        <input
+          className="w-full px-4 py-2 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+          placeholder="Open source developer coding on ens"
+        />
+        {/* Location */}
+        <div className="flex flex-row justify-between">
+          <div className="mb-2 text-sm font-bold text-brownblack-700">
+            Location
+          </div>
+          <div className="text-sm text-red-500">{nameErrorMsg}</div>
+        </div>
+        <input
+          className="w-full px-4 py-2 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+          placeholder="New York City"
+        />
+      </div>
+    ),
+    links: (
+      <div className="flex flex-col gap-2 mt-4">
+        <div className="flex flex-row justify-between ">
+          <div className="mb-2 text-sm font-bold text-brownblack-700">
+            Links
+          </div>
+        </div>
+        {/* Website */}
+        <div className="relative">
+          <Image
+            src="/images/icon-link.png"
+            width={18}
+            height={18}
+            alt="x"
+            className="absolute top-3 left-3"
+          ></Image>
+          <input
+            className="w-full px-4 py-2 pl-10 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+            placeholder="https://"
+          />
+        </div>
+        {/* X */}
+        <div className="relative">
+          <Image
+            src="/images/logo-x-black.png"
+            width={18}
+            height={18}
+            alt="x"
+            className="absolute top-3 left-3"
+          ></Image>
+          <input
+            className="w-full px-4 py-2 pl-10 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+            placeholder="@namestonehq"
+          />
+        </div>
+        {/* Github */}
+        <div className="relative">
+          <Image
+            src="/images/logo-github-brown.svg"
+            width={18}
+            height={18}
+            alt="x"
+            className="absolute top-3 left-3"
+          ></Image>
+          <input
+            className="w-full px-4 py-2 pl-10 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+            placeholder="@resolverworks"
+          />
+        </div>
+        {/* Discord */}
+        <div className="relative">
+          <Image
+            src="/images/logo-discord.png"
+            width={18}
+            height={18}
+            alt="x"
+            className="absolute top-3 left-3"
+          ></Image>
+          <input
+            className="w-full px-4 py-2 pl-10 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+            placeholder="@namestonehq"
+          />
+        </div>
+        {/* Telegram */}
+        <div className="relative">
+          <Image
+            src="/images/logo-telegram.png"
+            width={18}
+            height={18}
+            alt="x"
+            className="absolute top-3 left-3"
+          ></Image>
+          <input
+            className="w-full px-4 py-2 pl-10 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+            placeholder="@namestonehq"
+          />
+        </div>
+        {/* IPFS */}
+        <div className="relative">
+          <Image
+            src="/images/logo-ipfs.png"
+            width={18}
+            height={18}
+            alt="x"
+            className="absolute top-3 left-3"
+          ></Image>
+          <input
+            className="w-full px-4 py-2 pl-10 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+            placeholder="@namestonehq"
+          />
+        </div>
+      </div>
+    ),
+    addresses: (
+      <div className="flex flex-col gap-2 mt-4">
+        <div className="text-sm font-bold text-brownblack-700">Bitcoin</div>
+        {/* Bitcoin */}
+        <div className="relative">
+          <Image
+            src="/images/logo-bitcoin.svg"
+            width={18}
+            height={18}
+            alt="x"
+            className="absolute top-3 left-3"
+          ></Image>
+          <input
+            className="w-full px-4 py-2 pl-10 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+            placeholder="bc1q...aw4n"
+          />
+        </div>
+        {/* Solana */}
+        <div className="text-sm font-bold text-brownblack-700">Solana</div>
+        <div className="relative">
+          <Image
+            src="/images/logo-solana.svg"
+            width={18}
+            height={18}
+            alt="x"
+            className="absolute top-3 left-3"
+          ></Image>
+          <input
+            className="w-full px-4 py-2 pl-10 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+            placeholder="Ge83...S2bh"
+          />
+        </div>
+        {/* Base */}
+        <div className="text-sm font-bold text-brownblack-700">Base</div>
+        <div className="relative">
+          <Image
+            src="/images/logo-base.svg"
+            width={18}
+            height={18}
+            alt="x"
+            className="absolute top-3 left-3"
+          ></Image>
+          <input
+            className="w-full px-4 py-2 pl-10 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+            placeholder="0x5346...D42CF"
+          />
+        </div>
+        {/* Optimism */}
+        <div className="text-sm font-bold text-brownblack-700">Optimism</div>
+        <div className="relative">
+          <Image
+            src="/images/logo-op.svg"
+            width={18}
+            height={18}
+            alt="x"
+            className="absolute top-3 left-3"
+          ></Image>
+          <input
+            className="w-full px-4 py-2 pl-10 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+            placeholder="0x5346...D42CF"
+          />
+        </div>
+        {/* Scroll */}
+        <div className="text-sm font-bold text-brownblack-700">Scroll</div>
+        <div className="relative">
+          <Image
+            src="/images/logo-scroll.svg"
+            width={18}
+            height={18}
+            alt="x"
+            className="absolute top-3 left-3"
+          ></Image>
+          <input
+            className="w-full px-4 py-2 pl-10 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+            placeholder="0x5346...D42CF"
+          />
+        </div>
+        {/* Optimism */}
+        <div className="text-sm font-bold text-brownblack-700">arbitrum</div>
+        <div className="relative">
+          <Image
+            src="/images/logo-arb.svg"
+            width={18}
+            height={18}
+            alt="x"
+            className="absolute top-3 left-3"
+          ></Image>
+          <input
+            className="w-full px-4 py-2 pl-10 mb-4 border rounded-md ring-1 ring-gray-300 border-brownblack-50 focus:ring-2 focus:ring-orange-400 focus:outline-none"
+            placeholder="0x5346...D42CF"
+          />
+        </div>
+      </div>
+    ),
+    custom: <div className="flex flex-col mt-4">{/* Custom content */}</div>,
+  };
+
+  const SUBNAME_CONTENT = (
+    <>
+      <Dialog.Title className="text-base font-bold text-brownblack-700">
+        {modalType === "add" ? "Add a subname" : "Edit Subname"}
+      </Dialog.Title>
+      <hr className="border-0 h-[0.5px] bg-brownblack-200/50 my-4"></hr>
+      <div className="flex flex-col">
+        <div className="flex flex-row justify-between">
+          <div className="text-sm font-bold text-brownblack-700">Subname</div>
+          <div className="text-sm text-red-500">{nameErrorMsg}</div>
+        </div>
+        <input
+          className="w-full px-4 py-2 mb-4 border rounded-md border-brownblack-50"
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+        />
+        <div className="flex flex-row justify-between">
+          <div className="text-sm font-bold text-brownblack-700">Address</div>
+          <div className="text-sm text-red-500">{addressErrorMsg}</div>
+        </div>
+        <input
+          className="w-full px-4 py-2 mb-4 font-mono text-sm border rounded-md border-brownblack-50"
+          value={addressInput}
+          onChange={(e) => setAddressInput(e.target.value)}
+        />
+      </div>
+    </>
+  );
+
   return (
     <Dialog
       className="relative z-50"
       open={open}
       onClose={() => setOpen(false)}
     >
-      {/* The backdrop, rendered as a fixed sibling to the panel container */}
       <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-
       <div className="fixed inset-0 flex items-center justify-center p-4">
-        <Dialog.Panel className="w-full max-w-[496px] px-6 py-4 bg-white rounded-lg ">
-          <Dialog.Title className="text-base font-bold text-brownblack-700">
-            {modalType === "add" ? "Add a subname" : "Edit Subname"}
-          </Dialog.Title>
-          <hr className="border-0 h-[0.5px] bg-brownblack-200/50 my-4"></hr>
-          <button
-            className="flex items-center gap-1 mt-2 ml-auto text-xs text-orange-800 transition-colors duration-300 hover:text-orange-600"
-            onClick={() => {}}
-          >
-            More Records <span className="text-sm">&gt;</span>
-          </button>
-          <div className="flex flex-col">
-            <div className="flex flex-row justify-between">
-              <div className="text-sm font-bold text-brownblack-700">
-                Subname
+        <Dialog.Panel className="w-full max-w-[496px] px-6 py-4 bg-white rounded-lg">
+          {!showSubname ? (
+            <>
+              <button
+                className="flex items-center gap-1 mt-2 mb-8 mr-auto text-xs text-orange-800 transition-colors duration-300 hover:text-orange-400"
+                onClick={() => setShowSubname(true)}
+              >
+                <span className="text-sm">&lt;</span> name.name.eth
+              </button>
+
+              <div className="flex gap-6 mt-2 text-neutral-400">
+                {tabs.map((tab) => (
+                  <button
+                    key={tab}
+                    className={`relative border-b-2 transition-colors duration-300 pb-2
+                      ${
+                        activeTab === tab
+                          ? "text-orange-500 border-orange-500"
+                          : "border-transparent hover:border-orange-400 hover:text-orange-400"
+                      }`}
+                    onClick={() => setActiveTab(tab)}
+                  >
+                    {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                  </button>
+                ))}
               </div>
-              <div className="text-sm text-red-500">{nameErrorMsg}</div>
-            </div>
-            <input
-              className="w-full px-4 py-2 mb-4 border rounded-md border-brownblack-50"
-              value={nameInput}
-              onChange={(e) => setNameInput(e.target.value)}
-            />
-            <div className="flex flex-row justify-between">
-              <div className="text-sm font-bold text-brownblack-700">
-                Address
-              </div>
-              <div className="text-sm text-red-500">{addressErrorMsg}</div>
-            </div>
-            <input
-              className="w-full px-4 py-2 mb-4 font-mono text-sm border rounded-md border-brownblack-50"
-              value={addressInput}
-              onChange={(e) => setAddressInput(e.target.value)}
-            />
-          </div>
+              <hr className="-mt-[1px] bg-neutral-200" />
+
+              {TAB_CONTENT[activeTab]}
+            </>
+          ) : (
+            SUBNAME_CONTENT
+          )}
+
           <div className="flex justify-between w-full">
             <button className="flex items-end mb-2 ml-1 text-sm font-bold text-red-500 transition-colors duration-300 hover:text-red-600 hover:cursor-pointer">
               Delete
