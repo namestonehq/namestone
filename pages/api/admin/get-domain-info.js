f4b1ebed1c35633f08a707a05d83713aceab0754
Darian Bailey <darian@nftychat.xyz>
2025-03-17 14:27:49 -0400

    fix for admin domain text records

--- pages/api/admin/get-domain-info.js
+++ pages/api/admin/get-domain-info.js
@@ -1,28 +1,29 @@
 import sql from "../../../lib/db";
-import { getToken } from "next-auth/jwt";
+import { getAdminTokenById } from "../../../utils/ServerUtils";
 
 export default async function handler(req, res) {
-  const token = await getToken({ req });
-
-  if (!token) {
-    return res.status(401).json({ error: "Unauthorized. Please refresh." });
-  }
-
-  const superAdminQuery = await sql`
-  SELECT * FROM super_admin WHERE address = ${token.sub};
-`;
-  if (superAdminQuery.length === 0) {
-    return res.status(401).json({ error: "Unauthorized. Please refresh." });
-  }
-  // Get Domain && Name
+  // check Domain
   if (!req.query.domain_id) {
     return res.status(400).json({ error: "Domain is required" });
+  }
+
+  // check Admin permissions
+  const adminToken = await getAdminTokenById(req, req.query.domain_id);
+
+  if (!adminToken) {
+    return res
+      .status(401)
+      .json({ error: "You are not authorized to use this endpoint" });
   }
 
   const domainQuery = await sql`
   select domain.id, domain.name, domain.address, domain.contenthash_raw as contenthash
   from domain
   where domain.id = ${req.query.domain_id}`;
+
+  if (domainQuery.length == 0) {
+    return res.status(401).json({ error: "Domain not found" });
+  }
 
   // get text records from db
   const textRecordQuery = await sql`
